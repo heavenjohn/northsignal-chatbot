f8e39f8b5d8643c04a96578ea6e18e3e2f8f3401
heavenjohn <johnheaven48@gmail.com>
2025-01-10 20:12:37 +0800

    update3

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import axios from "axios";
 
-// axios.defaults.baseURL = "http://localhost:3000";
-axios.defaults.baseURL = window.location.origin;
+axios.defaults.baseURL = "http://localhost:3000";
+//axios.defaults.baseURL = window.location.origin;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
