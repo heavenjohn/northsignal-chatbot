193a28108d764491f33333edd0fba3c30e07371a
heavenjohn <johnheaven48@gmail.com>
2025-01-11 16:19:04 +0800

    test again

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import axios from "axios";
+
+//axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = window.location.origin;
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
